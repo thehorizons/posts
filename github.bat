ffc7fc7fc9708a78c935bda973c269fa18cccf4e
thehorizons <yongfaz@hotmail.com>
2017-10-27 13:56:14 +0800

    rebuilding site

--- github.bat
+++ github.bat
@@ -2,10 +2,12 @@ hugo
 cd www
 git pull origin master
 git add .
+git status
 git commit -a -m "rebuilding site"
 git push origin master
 cd ..
 
 git add .
+git status
 git commit -a -m "rebuilding site"
 git push origin master
